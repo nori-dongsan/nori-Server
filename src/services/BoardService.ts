b33952747afe010b4fbc1e3154fc163e61fd2b93
gunom <gu3062@naver.com>
2022-07-16 16:55:35 +0900

    refactor: board List response 값 수정

--- src/services/BoardService.ts
+++ src/services/BoardService.ts
@@ -1,5 +1,6 @@
 import { Service } from "typedi";
 import { InjectRepository } from "typeorm-typedi-extensions";
+import { BoardCreateDto } from "../dtos/BoardDto";
 import { Board } from "../entities/Board";
 import { BoardRepository } from "../repositories/BoardRepository";
 
@@ -13,7 +14,15 @@ export class BoardService {
     public async getList(): Promise<Board[]> {
         return await this.boardRepository.find({
             skip: 0,
-            take: 10
+            take: 10,
+            select: ["id", "title", "content"],
+            relations: ["user"]
         })
+    }
+
+    public async create(boardCreateDto: BoardCreateDto): Promise<Board> {
+        const board = this.boardRepository.create(boardCreateDto)
+        await this.boardRepository.save(board)
+        return board
     }
 }
