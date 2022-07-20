071de2201ff32ef549521f57d2c7816cffb8d9a5
gunom <gu3062@naver.com>
2022-07-21 00:11:56 +0900

    Auto stash before merge of "develop" and "origin/develop"

--- src/controllers/BoardController.ts
+++ src/controllers/BoardController.ts
@@ -1,16 +1,16 @@
 import {
-  Delete,
-  Get,
-  HttpCode,
-  JsonController,
-  Post,
-  Put,
-  Req,
-  Res,
-  UploadedFiles,
-  UseBefore,
-  QueryParam,
-  Param,
+    Delete,
+    Get,
+    HttpCode,
+    JsonController,
+    Post,
+    Put,
+    Req,
+    Res,
+    UploadedFiles,
+    UseBefore,
+    QueryParam,
+    Param,
 } from 'routing-controllers';
 import { OpenAPI } from 'routing-controllers-openapi';
 import message from '../modules/responseMessage';
@@ -29,56 +29,35 @@ import { User } from '../entities/User';
 import { BoardImageService } from '../services/BoardImageService';
 import { BoardImageCreateDto } from '../dtos/BoardImageDto';
 import { logger } from '../utils/Logger';
-import { BoardCommentService } from "../services/BoardCommentService";
 
 @JsonController('/board')
 export class BoardController {
-  constructor(
-    private boardService: BoardService,
-    private boardCommentService: BoardCommentService,
-    private userService: UserService,
-    private boardImageService: BoardImageService
-  ) {}
+    constructor(
+        private boardService: BoardService,
+        private boardCommentService: BoardCommentService,
+        private userService: UserService,
+        private boardImageService: BoardImageService
+    ) { }
 
-  @HttpCode(200)
-  @Get('')
-  @OpenAPI({
-    summary: '게시판 목록 조회',
-    description: '게시물 리스트 반환',
-    statusCode: '200',
-  })
-  public async getList(
-    @Res() res: Response,
-    @QueryParam('page') page: number
-  ): Promise<Response> {
-    const boards = await this.boardService.getList(page);
+    @HttpCode(200)
+    @Get('')
+    @OpenAPI({
+        summary: '게시판 목록 조회',
+        description: '게시물 리스트 반환',
+        statusCode: '200',
+    })
+    public async getList(
+        @Res() res: Response,
+        @QueryParam('page') page: number
+    ): Promise<Response> {
+        const boards = await this.boardService.getList(page);
 
-    return res
-      .status(statusCode.CREATED)
-      .send(
-        util.success(statusCode.OK, message.READ_BAORD_LIST_SUCCESS, boards)
-      );
-  }
-
-  @UseBefore(verifyAccessToken)
-  @HttpCode(200)
-  @Get('/:boardId')
-  @OpenAPI({
-    summary: '게시글 조회',
-    description: '게시글 반환',
-    statusCode: '200',
-  })
-  public async get(
-    @Res() res: Response,
-    @Param('boardId') boardId: number
-  ): Promise<Response> {
-        const { id } = res.locals.jwtPayload;
-    const board = await this.boardService.get(boardId);
-    let author = false;
-    if (board?.user.id == id) {
-      author = true;
+        return res
+            .status(statusCode.CREATED)
+            .send(
+                util.success(statusCode.OK, message.READ_BAORD_LIST_SUCCESS, boards)
+            );
     }
-
     @UseBefore(verifyAccessToken)
     @HttpCode(201)
     @Post("")
