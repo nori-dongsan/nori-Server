ff3127eebd9334ab5066df46b200ddb610b20663
Crayon <lsh328328@naver.com>
2022-07-22 20:55:23 +0900

    CORS 에러 처리

--- src/utils/RoutingConfig.ts
+++ src/utils/RoutingConfig.ts
@@ -3,7 +3,11 @@ import { env } from '../env';
 export const routingControllerOptions = {
   cors: {
     credentials: true,
-    origin: ['http://localhost:3000', `${env.app.ec2Instance}`],
+    origin: [
+      'http://localhost:3000',
+      `${env.app.ec2Instance}`,
+      'https://www.with-nori.com',
+    ],
   },
   routePrefix: env.app.apiPrefix,
   controllers: [`${__dirname}/../controllers/*{.ts,.js}`],
